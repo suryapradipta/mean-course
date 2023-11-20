7c595946ad16bf80baf1c6387aa7344608ceb231
suryapradipta.github.io <63577853+suryapradipta@users.noreply.github.com>
2023-11-20 14:27:06 +0800

    L9 process1: installation express

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const app = require('./backend/app');
+
+const port = 3000;
+app.set('port', port);
+const server = http.createServer(app);
+
+server.listen(port, ()=>{
+  console.log('Server running');
+});
